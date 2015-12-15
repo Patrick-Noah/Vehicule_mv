83e8605b8f23cb29d6b0a9fa675cbcde3c8b4747
shelby <shelby@kali>
2015-12-15 11:53:12 +0100

    new commit

---
+++ src/main/java/com/shelby/syl20/vehicule/TestVechicule.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shelby.syl20.vehicule;
+
+/**
+ *
+ * @author shelby
+ */
+public class TestVechicule {
+    public static void main(String[] args) {
+        Vehicules monVehicule = new Vehicules();
+        monVehicule.demarrer();
+        monVehicule.accelerer(12); 
+    }
+    
+}
